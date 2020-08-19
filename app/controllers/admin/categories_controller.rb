18b158806447594bc158e77edc6f18d9bb0565ea
Alyssa Beaton <ehseebe@gmail.com>
2020-08-19 22:06:09 +0000

    Created Admin Category controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,3 +1,36 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV["ADMIN_LOGIN"], password: ENV["ADMIN_PASSWORD"]
+
+  def index 
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else 
+      render: new
+    end
+  end
+
+  def destroy
+    @category = Category.find_by_name(params[:name])
+    @category.destroy
+    redirect_to [:admin, :categories], notice: 'Category deleted!'
+  end
+
+  private 
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
   
 end
