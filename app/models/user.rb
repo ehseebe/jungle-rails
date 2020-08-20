031533eaf1c73ef9f2c93e5fc640d1b720b76d35
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-20 13:44:47 -0400

    Validation for AR input of User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
 
   has_secure_password 
   
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :password_digest, presence: true, length: { minimum: 6 }
+
 end
