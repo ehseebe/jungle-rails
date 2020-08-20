411750bef1f319512335d18e517cadd35d245f3f
Alyssa Beaton <ehseebe@gmail.com>
2020-08-20 14:37:55 +0000

    Implemented sessions controller, create and destroy methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    # checks user exists + password is correct
+    if user && user.authenticate(params[:password])
+      # saves user id in browser cookie so user can navigate freely
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+
+end
