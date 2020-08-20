5a89e538ed3c6c0839f0de530757a29efa3a221d
Alyssa Beaton <ehseebe@gmail.com>
2020-08-20 00:06:20 +0000

    Created about page

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,7 @@
+class AboutController < ApplicationController
+
+  def show
+    render about
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  get '/about' => 'about#about'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
