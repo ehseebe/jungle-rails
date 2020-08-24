04ad802757e0efb9385d618f1dbf9177ef599c0f
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-24 17:09:36 -0400

    First test passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "Validations" do
-    it ""
+    it "is valid with valid attributes" do
+      user = User.new(email: "alyssa@sidewalkplants.com", first_name: "Alyssa", last_name: "BT", password: "apple!123", password_confirmation: "apple!123")
+    end
   end
 
   describe ".authenticate_with_credentials" do
