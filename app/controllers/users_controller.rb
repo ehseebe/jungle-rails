749b203e740f9f524d86a0bf488ac55d432ba461
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-24 16:37:15 -0400

    New authentication class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    # user = User.new(user_params)
+    # if user.save
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
