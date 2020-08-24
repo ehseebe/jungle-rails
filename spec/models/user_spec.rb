ed2a0cf0885c243b742bc37e1eeb37b103110545
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-24 17:15:19 -0400

    Two tests passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,14 @@ RSpec.describe User, type: :model do
   describe "Validations" do
     it "is valid with valid attributes" do
       user = User.new(email: "alyssa@sidewalkplants.com", first_name: "Alyssa", last_name: "BT", password: "apple!123", password_confirmation: "apple!123")
+      expect(user).to be_valid
     end
+
+    it "is not valid without an email" do
+      user = User.new(email: nil, first_name: "Alyssa", last_name: "BT", password: "apple!123", password_confirmation: "apple!123")
+      expect(user).to_not be_valid
+    end
+    
   end
 
   describe ".authenticate_with_credentials" do
