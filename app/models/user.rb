f97f3637a752c0b3145f8bc80e5ae74fd191c573
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-24 17:32:01 -0400

    Removed digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
 
   has_secure_password 
   
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :password_digest, presence: true, confirmation: true, length: { minimum: 6 }
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
 
   def authenticate_with_credentials(email, password) 
 
