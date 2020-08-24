eecaee9c1228b5892bd4c4a1fe7b078b57c34e00
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-24 17:09:11 -0400

    Added method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :password_digest, presence: true, confirmation: true, length: { minimum: 6 }
 
+  def authenticate_with_credentials(email, password) 
+
+  end
+
 end
