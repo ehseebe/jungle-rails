36c7e6f94d8cc8e23f6fe24a40dab4d244e9de4c
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-24 15:35:34 -0400

    Test outlines

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  
+  describe 'Validations' do
+    it "is valid with valid attributes" do
+      
+    end
+    it "is not valid without a name" do
+      
+    end
+    it "is not valid without a price" do
+      
+    end
+    it "is not valid without a quantity" do
+      
+    end
+    it "is not valid without a category" do
+      
+    end
+
+  end
+end
