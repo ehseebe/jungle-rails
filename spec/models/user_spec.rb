8c45a2c244a0838aea403aedee9a7c8daad0f340
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-25 11:51:37 -0400

    Refined downcase testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -60,15 +60,19 @@ RSpec.describe User, type: :model do
 
       expect(user).to be_valid
     end
-    it "is valid regardless of email letter case" do
+    it "makes email letters downcase" do
       user = User.new(email: "alySSa@SidewalkPlants.com", first_name: "Alyssa", last_name: "BT", password: "apple!123", password_confirmation: "apple!123")
 
-      expect(user).to be_valid
-    end
-    it "is valid regardless of whitespace around email" do
-      user = User.new(email: " alySSa@SidewalkPlants.com ", first_name: "Alyssa", last_name: "BT", password: "apple!123", password_confirmation: "apple!123")
+      user.save
 
-      expect(user).to be_valid
+      expect(user[:email]).to be == ("alyssa@sidewalkplants.com")
+    end
+    it "trims whitespace around email" do
+      user = User.new(email: " alyssa@sidewalkplants.com ", first_name: "Alyssa", last_name: "BT", password: "apple!123", password_confirmation: "apple!123")
+
+      user.save
+
+      expect(user[:email]).to be ==("alyssa@sidewalkplants.com")
     end
   end
 end
