c318321bfc4f50fc988e44a36756aae22517cc70
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-25 11:26:13 -0400

    Tried strip_attributes, not for rails4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "Validations" do
+
     it "is valid with valid attributes" do
       user = User.new(email: "alyssa@sidewalkplants.com", first_name: "Alyssa", last_name: "BT", password: "apple!123", password_confirmation: "apple!123")
       expect(user).to be_valid
