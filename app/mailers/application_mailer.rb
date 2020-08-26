994f1b84fab85e61d6e04c219dc1d93a8fc7c78d
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-26 14:49:54 -0400

    Set up mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no-reply@jungle.com"
   layout 'mailer'
+
+  def order_confirmation_email(user, order)
+    @order = order
+    @user = user
+    @url = "http://0.0.0.0:3000/"
+    mail(to: @user.email, subject: "Your Jungle order ##{@order.id} is confirmed 🌴")
+  end
 end
