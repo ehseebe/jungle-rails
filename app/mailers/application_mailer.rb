5f3f56b00c67d379ef3fd56172194dad7ec0ecfd
Alyssa Beaton <ehseebe@gmail.com>
2020-08-26 18:52:40 +0000

    Moved to UserMailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,11 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "no-reply@jungle.com"
   layout 'mailer'
-
-  def order_confirmation_email(user, order)
-    @order = order
-    @user = user
-    @url = "http://0.0.0.0:3000/"
-    mail(to: @user.email, subject: "Your Jungle order ##{@order.id} is confirmed 🌴")
-  end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+  default from: "no-reply@jungle.com"
+
+  def order_confirmation_email(user, order)
+    @order = order
+    @user = user
+    @url = "http://0.0.0.0:3000/"
+    mail(to: @user.email, subject: "Your Jungle order ##{@order.id} is confirmed 🌴")
+  end
 end
